ec187c1adc23035da9d78fa4a10e7c5cf0ae0aa9
Sergio Baeza Carrasco <svbc2@alu.ua.es>
2023-08-03 20:13:36 +0200

    Added base model

--- fresco/settings.py
+++ fresco/settings.py
@@ -23,12 +23,12 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "safedelete",
     "ninja_extra",
 ]
 
